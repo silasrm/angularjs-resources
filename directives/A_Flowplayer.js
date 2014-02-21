72a49a43ec4c1b33919f2b67229ad86d700f0a15
Silas Ribas Martins <silasrm@gmail.com>
2014-02-21 01:58:22 -0200

    Update A_Flowplayer.js

--- directives/A_Flowplayer.js
+++ directives/A_Flowplayer.js
@@ -9,7 +9,7 @@
 // ## Add scripts:                                                          ## //
 // ## <script src="/js/vendor/flowplayer/flowplayer-3.1.4.min.js"></script> ## //
 // ##                                                                       ## //
-// ## <a href="[[ item.file ]]" flowplayer flowplayer-swf-url="/js/vendor/flowplayer/flowplayer-3.2.1.swf" flowplayer-callback="notifyFinishedContent(item.id)"></a>  ## //
+// ## <a href="[[ item.file ]]" flowplayer flowplayer-swf-url="/js/vendor/flowplayer/flowplayer-3.2.1.swf" flowplayer-callback="notifyFinishedContent(item.id)" flowplayer-background-color="#BDBDBB" flowplayer-progress-color="#BDBDBB" flowplayer-buffer-color="#524E4D" flowplayer-button-color="#333333" flowplayer-fullscreen="false" flowplayer-height="30" flowplayer-autoHide="false"></a>  ## //
 // ########################################################################### //
 define(
     [
@@ -32,18 +32,18 @@ define(
                                 {
                                     plugins: {
                                         controls: {
-                                            backgroundColor: '#BDBDBB',
-                                            progressColor: '#BDBDBB',
-                                            bufferColor: '#524E4D',
-                                            buttonColor: '#333333',
-                                            fullscreen: false,
-                                            height: 30,
-                                            autoHide: false
+                                            backgroundColor: attrs.flowplayerBackgroundColor || '#BDBDBB',
+                                            progressColor: attrs.flowplayerProgressColor || '#BDBDBB',
+                                            bufferColor: attrs.flowplayerBufferColor || '#524E4D',
+                                            buttonColor: attrs.flowplayerButtonColor || '#333333',
+                                            fullscreen: attrs.flowplayerFullscreen || false,
+                                            height: attrs.flowplayerHeight || 30,
+                                            autoHide: attrs.flowplayerAutoHide || false
                                         }
                                     },
                                     clip: {
                                         onFinish: function() {
-                                            if(typeof flowplayerCallback !== 'undefined') {
+                                            if(attrs.flowplayerCallback && typeof flowplayerCallback !== 'undefined') {
                                                 flowplayerCallback();
                                             }
                                         }
